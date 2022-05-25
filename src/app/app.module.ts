cc5ce44934ccde1d823c1bb4c9136aa19509a5c6
manu1101 <manu_1101@hotmail.com>
2022-05-25 20:02:20 -0300

    Creación Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IniciarSesionComponent
   ],
   imports: [
     BrowserModule
