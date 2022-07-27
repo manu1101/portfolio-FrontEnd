a317edd7a27493179a6a2363e0e7ef7cd5000ff9
manu1101 <manu_1101@hotmail.com>
2022-07-27 00:49:15 -0300

    Intento 1 millon

--- src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
@@ -13,7 +13,7 @@ export class IniciarSesionComponent implements OnInit {
   constructor(private formBuilder:FormBuilder, private autenticacionService:AutenticacionService, private ruta:Router) {
     this.form = this.formBuilder.group(
       {
-        email:['',[Validators.required, Validators.email]],
+        username:['',[Validators.required, Validators.email]],
         password:['',[Validators.required, Validators.minLength(8)]]
       })
    }
