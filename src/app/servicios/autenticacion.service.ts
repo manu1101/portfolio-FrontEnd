260381449626ac441f8caa32e0b71d5a34011775
manu1101 <manu_1101@hotmail.com>
2022-10-22 18:43:35 -0300

    Cambios de cors

--- src/app/servicios/autenticacion.service.ts
+++ src/app/servicios/autenticacion.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://localhost:8080/api/login/';
+const AUTH_API = 'https://arg-prog.herokuapp.com/api/login/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
